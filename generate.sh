c11151c8f870b99ba3df140932bbd2286d5a4b45
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-14 13:16:49 +0100

    update

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 $(cd ansi16/ && bash generate.sh)
-cp -rf ansi16/output/ -T themes/
+cp -f ansi16/*.tmTheme themes/
 python3 ansi16/yamltotm/yamltotm.py templates/synthwave.json -d ansi16/palettes/material-dark.yaml -r themes/synthwave-material-ansi16-color-theme.json
 python3 ansi16/yamltotm/yamltotm.py templates/noctis-lux.json -d ansi16/palettes/noctis-lux.yaml -r themes/noctis-lux-ansi16-color-theme.json
